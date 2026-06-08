87365a31241b5b46b590858e746befd062a5bdad
Bernardo <berramuspegonzalez@gmail.com>
2026-06-08 00:25:38 -0300

    feat: add sitemap, robots.txt, SEO-optimized layout, and environment variable support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bernard2806.is-a.dev',
   base: '/',
   output: 'static',
+
   build: {
     inlineStylesheets: 'always'
-  }
+  },
+
+  integrations: [sitemap()]
 });
